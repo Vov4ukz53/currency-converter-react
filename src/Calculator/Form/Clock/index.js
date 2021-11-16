2d335de80188ec8d373ee248403665b71bce242a
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-11-16 20:39:52 +0100

    created formateDate to optimize the code

--- src/Calculator/Form/Clock/index.js
+++ src/Calculator/Form/Clock/index.js
@@ -1,27 +1,33 @@
 import { useEffect, useState } from "react";
 import "./style.css";
 
+const formateDate = (date) =>
+   date.toLocaleString("pl", {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric"
+   });
+
 export const Clock = () => {
    const [date, setDate] = useState(new Date());
 
    useEffect(() => {
       const intervalId = setInterval(() => {
-         setDate(new Date())
+         setDate(new Date());
       }, 1000);
+
       return () => {
          clearInterval(intervalId);
-      }
+      };
    }, []);
 
    return (
       <p className="clock">
-         Disiaj jest&nbsp;
-         {date.toLocaleString("pl",
-            { weekday: "long" })},&nbsp;
-         {date.toLocaleString("pl",
-            { day: "numeric", month: "long" })},&nbsp;
-         {date.toLocaleString("pl",
-            { hour: "numeric", minute: "numeric", second: "numeric" })}
+         Disiaj jest
+         {formateDate(date)}
       </p>
-   )
+   );
 };
