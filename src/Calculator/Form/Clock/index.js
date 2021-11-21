f47437c4790034d6c2f0516a658da5494ebe0935
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-11-21 12:09:14 +0100

    created useCurrentDate

--- src/Calculator/Form/Clock/index.js
+++ src/Calculator/Form/Clock/index.js
@@ -1,28 +1,8 @@
-import { useEffect, useState } from "react";
+import { useCurrentDate, formateDate } from "./useCurrentDate";
 import "./style.css";
 
-const formateDate = (date) =>
-   date.toLocaleString("pl", {
-      weekday: "long",
-      day: "numeric",
-      month: "long",
-      hour: "numeric",
-      minute: "numeric",
-      second: "numeric"
-   });
-
 export const Clock = () => {
-   const [date, setDate] = useState(new Date());
-
-   useEffect(() => {
-      const intervalId = setInterval(() => {
-         setDate(new Date());
-      }, 1000);
-
-      return () => {
-         clearInterval(intervalId);
-      };
-   }, []);
+   const date = useCurrentDate();
 
    return (
       <p className="clock">
---
+++ src/Calculator/Form/Clock/useCurrentDate.js
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+
+export const formateDate = (date) =>
+   date.toLocaleString("pl", {
+      weekday: "long",
+      day: "numeric",
+      month: "long",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric"
+   });
+
+export const useCurrentDate = () => {
+   const [date, setDate] = useState(new Date());
+
+   useEffect(() => {
+      const intervalId = setInterval(() => {
+         setDate(new Date());
+      }, 1000);
+
+      return () => {
+         clearInterval(intervalId);
+      };
+   }, []);
+
+   return date;
+};
