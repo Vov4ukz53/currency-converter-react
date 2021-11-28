16c301f5f2a732b77788c406d6cc8ba2d5a3d81e
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-11-28 18:11:53 +0100

    deleted currencies

--- src/currencies.js
+++
@@ -1,8 +0,0 @@
-
-const currencies = [
-    { content: "eur", name: "Euro", rate: 0.22 },
-    { content: "usd", name: "USD - dolar amerykański", rate: 0.25 },
-    { content: "gbp", name: "GBP - Funt szterling", rate: 0.19 },
-  ];
-
-export default currencies;
