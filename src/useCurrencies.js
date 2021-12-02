b4889f6a9de7bea557ab5f03222857e207cce992
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-12-02 16:35:47 +0100

    added state: "loading"

--- src/useCurrencies.js
+++ src/useCurrencies.js
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 export const useCurrencies = () => {
-   const [currenciesData, setCurrenciesData] = useState({});
+   const [currenciesData, setCurrenciesData] = useState({ state: "loading" });
 
    useEffect(() => {
       const getCurrencies = async () => {
