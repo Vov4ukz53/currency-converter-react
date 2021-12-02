74bd1aba508168a3c91756ac17a23f5e9d6e66b0
Volodymyr Bronevych <Vov4ukz53@gmail.com>
2021-12-02 16:37:11 +0100

    removed animation

--- src/Calculator/Result/styled.js
+++ src/Calculator/Result/styled.js
@@ -6,40 +6,10 @@ export const Wrapper = styled.div`
 	text-align: center;
 `;
 
-const scale = keyframes`
-	0% {
-      transform: scale(1); 
-   }
-
-   50% {
-      transform: scale(1.1); 
-   }
-
-   100% {
-      transform: scale(1); 
-   }
-`;
-
-const color = keyframes`
-	0% {
-      color: #fff;
-   }
-
-   50% {
-      color: #105a23; 
-   }
-
-   100% {
-      color: #fff;
-   }
-`;
-
 export const ResultMain = styled.p`
    font-size: 22px;
 	font-weight: 700;
 	margin: 0px 0px 10px 0px;
-   animation: ${scale} 2s linear infinite,
-      ${color} 2s linear infinite;
 `;
 
 export const ResultInfo = styled.p`
